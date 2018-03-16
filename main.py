c6d451e2d7ab6ff0078fafea46841ebdce441e21
Wilder Lizama <wilderlizama@gmail.com>
2018-03-15 22:37:27 -0500

    Uso de Jinja2 para templates

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*- #
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hola():
-    return '¡Hola, desde SERVIDOR WEB ✌ !'
+    return render_template("contact_book.html")
 
 
 if __name__ == '__main__':
