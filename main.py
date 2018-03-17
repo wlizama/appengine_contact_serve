79e68e9d90b6c154619f209cae777cca7bdbd5e8
Wilder Lizama <wilderlizama@gmail.com>
2018-03-16 22:01:31 -0500

    Mejora en lista de Contactos y enlace a vista de contacto

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from contact_model import Contact
 app = Flask(__name__)
+app.secret_key="some_secret"
+app.debug = True
 
 
 @app.route(r'/', methods=['GET'])
 def contact_book():
-    return render_template('contact_book.html')
+    contacts = Contact.query().fetch()
+    return render_template('contact_book.html', contacts=contacts)
 
 
 @app.route(r'/add', methods=['GET', 'POST'])
@@ -19,6 +22,7 @@ def add_contact():
                           email=request.form.get('email'))
 
         contact.put()
+        flash("Se añadió contacto: {}".format(request.form.get('name')))
 
     return render_template('add_contact.html')
 
